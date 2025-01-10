f785c66db1d3c660907bcb4b67b0aa3c1c229162
NELSON ANDRES LUENGAS SILVA <90053867+NELSONLUENGAS@users.noreply.github.com>
2025-01-09 19:07:44 -0500

    testing

--- src/config/db.js
+++ src/config/db.js
@@ -1,21 +1,16 @@
-require('dotenv').config()
-const { Pool } = require('pg')
+require('dotenv').config();
+const { Pool } = require('pg');
 
-const {
-    DB_HOST,
-    DB_USER,
-    DB_PASS,
-    DB_NAME
-} = process.env
+const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
 
 const DB = new Pool({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASS,
-    database: DB_NAME,
-    allowExitOnIdle: true
-})
+	host: DB_HOST,
+	user: DB_USER,
+	password: DB_PASS,
+	database: DB_NAME,
+	allowExitOnIdle: true,
+});
 
 module.exports = {
-    DB
-}
+	DB,
+};
--- src/models/Auth.js
+++ src/models/Auth.js
@@ -1,98 +1,107 @@
-const { DB } = require("../config/db");
+const { DB } = require('../config/db');
 const format = require('pg-format');
-const { comparePassword } = require("../helpers/bcrypt");
+const { comparePassword } = require('../helpers/bcrypt');
 
 const verifyCredentials = async (email, password) => {
-    try {
-        const SQLQuery = format(`
+	try {
+		const SQLQuery = format(
+			`
                 SELECT * FROM users
                 WHERE email = %L AND password = %L
             `,
-            email,
-            password
-        )
+			email,
+			password
+		);
 
-        const { rows: [user], rowCount } = await DB.query(SQLQuery)
+		const {
+			rows: [user],
+			rowCount,
+		} = await DB.query(SQLQuery);
 
-        if (!rowCount) {
-            throw new Error('NOT_FOUND')
-        } else {
-            return user
-        }
-
-    } catch (error) {
-        throw error
-    }
-}
+		if (!rowCount) {
+			throw new Error('USER_NOT_FOUND');
+		} else {
+			return Boolean(rowCount);
+		}
+	} catch (error) {
+		throw error;
+	}
+};
 
 const login = async (email, password) => {
-    try {
+	try {
+		// const user = await verifyCredentials(email, password)
 
-        // const user = await verifyCredentials(email, password)
+		const userExists = await exists(email);
 
-        const userExists = await exists(email)
+		const math = comparePassword(password, userExists.password);
 
-        const math = comparePassword(password, userExists.password)
-
-        if (!math) {
-            throw new Error('AUTH_ERROR')
-        } else {
-            return userExists
-        }
-    } catch (error) {
-        throw error
-    }
-}
-
+		if (!math) {
+			throw new Error('AUTH_ERROR');
+		} else {
+			return userExists;
+		}
+	} catch (error) {
+		throw error;
+	}
+};
 
 const exists = async (email) => {
-    try {
-        const SQLQuery = format(`
+	try {
+		const SQLQuery = format(
+			`
                 SELECT * FROM users
                 WHERE email = %L
             `,
-            email
-        )
+			email
+		);
 
-        const { rows: [user], rowCount } = await DB.query(SQLQuery)
+		const {
+			rows: [user],
+			rowCount,
+		} = await DB.query(SQLQuery);
 
-        if (!rowCount) {
-            throw new Error('NOT_FOUND')
-        } else {
-            return user
-        }
-
-    } catch (error) {
-        throw error
-    }
-}
+		if (!rowCount) {
+			throw new Error('NOT_FOUND');
+		} else {
+			return user;
+		}
+	} catch (error) {
+		throw error;
+	}
+};
 
 const register = async (email, password, role = 'USER') => {
-    try {
-
-        const SQLQuery = format(`
+	try {
+		const SQLQuery = format(
+			`
                 INSERT INTO users (email, password, role)
                 VALUES (%L, %L, %L)
                 RETURNING *
             `,
-            email, password, role
-        )
+			email,
+			password,
+			role
+		);
 
-        const { rows: [user], rowCount } = await DB.query(SQLQuery)
+		const {
+			rows: [user],
+			rowCount,
+		} = await DB.query(SQLQuery);
 
-        if (!rowCount) {
-            throw new Error('BAD_REQUEST')
-        } else {
-            return user
-        }
-    } catch (error) {
-        throw error
-    }
-}
+		if (!rowCount) {
+			throw new Error('BAD_REQUEST');
+		} else {
+			return user;
+		}
+	} catch (error) {
+		throw error;
+	}
+};
 
 module.exports = {
-    login,
-    register,
-    verifyCredentials,
-    exists
-}
+	login,
+	register,
+	verifyCredentials,
+	exists,
+};
